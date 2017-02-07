a5900cb28b43a5cfd9e40ed3509ae5302629dc9d
John Kotz <john.lyme@mac.com>
2017-02-07 17:35:08 -0500

    Adding beacon dependancy and set up index.ios.js using tutorial: https://github.com/frostney/react-native-ibeacon

--- index.ios.js
+++ index.ios.js
@@ -9,23 +9,58 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  DeviceEventEmitter
 } from 'react-native';
+var Beacons = require('react-native-ibeacon');
+
+// Define a region which can be identifier + uuid,
+// identifier + uuid + major or identifier + uuid + major + minor
+// (minor and major properties are numbers)
+var region = {
+    identifier: 'RadBeacon Dots',
+    uuid: 'F2363048-F649-4537-AB7E-4DADB9966544'
+};
+
+
+Beacons.requestWhenInUseAuthorization();
+Beacons.startMonitoringForRegion(region);
+
+Beacons.startRangingBeaconsInRegion(region);
+Beacons.startUpdatingLocation();
 
 export default class dali extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {beacons: null}
+  }
+
+  componentDidMount() {
+
+    var subscription = DeviceEventEmitter.addListener('beaconsDidRange',
+      (data) => {
+        this.setState({
+          beacons: data.beacons,
+        })
+    });
+  }
+
   render() {
+    let inRange = this.state.beacons != null && this.state.beacons.length;
+    var beaconNumText = null;
+
+    if (inRange) {
+      beaconNumText = <Text style={styles.instructions}>
+        In fact, there are {this.state.beacons.length} beacons nearby
+      </Text>
+    }
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          {inRange > 0 ? "There is a beacon nearby!" : "There are no beacons :("}
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        {beaconNumText}
       </View>
     );
   }
