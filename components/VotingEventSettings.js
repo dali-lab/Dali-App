e21a50e49eafc6b6b95245b1b127a681a8b505e7
John Kotz <john.lyme@mac.com>
2017-05-18 20:30:29 -0400

    Starting to style VoteEventSettings

--- components/VotingEventSettings.js
+++ components/VotingEventSettings.js
@@ -7,13 +7,14 @@ import {
    TouchableHighlight,
    ListView,
    Image,
+   Dimensions
 } from 'react-native';
 
 let ServerCommunicator = require('./ServerCommunicator').default;
 
 class VotingEventSettings extends Component {
    propTypes: {
-     navigator: React.PropTypes.Object.isRequired
+      navigator: React.PropTypes.Object.isRequired
    }
 
    constructor(props) {
@@ -23,11 +24,11 @@ class VotingEventSettings extends Component {
          event: null
       };
 
-      // ServerCommunicator.current.getEventNow().then((event) => {
-      //    this.setState({
-      //       event: event
-      //    })
-      // });
+      ServerCommunicator.current.getEventNow().then((event) => {
+         this.setState({
+            event: event
+         })
+      });
    }
 
    createPressed() {
@@ -37,16 +38,21 @@ class VotingEventSettings extends Component {
    render() {
       return (
          <View style={styles.container}>
-            {this.state.event == null ?
-               <Text>There is no event happening now. Create one below</Text>
-               :
-               <Text>There is an event happening now.</Text>
-            }
-
-            <TouchableHighlight
-            onPress={this.createPressed.bind(this)}>
-            <Text>Create</Text>
-            </TouchableHighlight>
+         <View style={styles.topView}>
+         {this.state.event == null ?
+            <Text style={styles.currentEventText}>There is no event happening now. Create one below</Text>
+            :
+            <View>
+            <Text style={styles.currentEventText}>Current Event:</Text>
+            </View>
+         }
+         </View>
+         <TouchableHighlight
+         underlayColor={'rgb(101, 161, 184)'}
+         style={styles.createButton}
+         onPress={this.createPressed.bind(this)}>
+         <Text style={styles.createButtonText}>Create</Text>
+         </TouchableHighlight>
          </View>
       );
    }
@@ -55,9 +61,29 @@ class VotingEventSettings extends Component {
 const styles = StyleSheet.create({
    container: {
       flex: 1,
-      backgroundColor: 'rgb(238, 238, 238)'
+      backgroundColor: 'rgb(238, 238, 238)',
+      alignItems: 'center'
    },
+   topView: {
+      flex: 1,
+   },
+   createButton: {
+      width: Dimensions.get('window').width,
+      alignItems: 'center',
+      padding: 10,
+      backgroundColor: 'rgb(116, 184, 209)'
+   },
+   createButtonText: {
+      color: 'white',
+      textAlign: 'center',
+      fontSize: 20,
+      fontFamily: "Avenir Next",
+      fontWeight: "500"
+   },
+   currentEventText: {
+      fontSize: 16,
 
+   }
 });
 
 module.exports = VotingEventSettings;
