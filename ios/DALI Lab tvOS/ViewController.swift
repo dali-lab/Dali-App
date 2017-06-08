cbf13775f47a00b25e2d1e91f7f1556e26925c27
John Kotz <john.lyme@mac.com>
2017-06-08 11:17:42 -0700

    Updating tvOS

--- ios/DALI Lab tvOS/ViewController.swift
+++ ios/DALI Lab tvOS/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
   @IBOutlet weak var peopleInLabLabel: UILabel!
   @IBOutlet weak var peopleInLabView: UIView!
   let wrapLabel = UILabel()
+  let fadeIn = #imageLiteral(resourceName: "Fadein")
+  let fadeOut = #imageLiteral(resourceName: "Fadeout")
+  let nonFaded = #imageLiteral(resourceName: "nonFaded")
   
   var events : [ServerCommunicator.Event]?
   
@@ -27,33 +30,56 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
       self.tableView.reloadData()
     }
     
-    let text = self.peopleInLabLabel.text!
-    self.peopleInLabLabel.text = text +  "  -  "
-    wrapLabel.textColor = self.peopleInLabLabel.textColor
-    wrapLabel.font = self.peopleInLabLabel.font
-    wrapLabel.text = text
-    wrapLabel.sizeToFit()
-    peopleInLabLabel.sizeToFit()
-    wrapLabel.frame = CGRect(x: self.peopleInLabLabel.frame.width, y: self.peopleInLabLabel.frame.origin.y, width: wrapLabel.frame.width, height: wrapLabel.frame.height)
+    let text = "People in the lab: John Kotz, Tim Tregubov, Andrew Beaubien, Jenny Seong, sjdkfla;skdjflk asjdfklja kl;sdfj;lajsd fl;kjasldkfa ;sx"
+    self.peopleInLabLabel.text = text
     
-    self.peopleInLabView.addSubview(wrapLabel)
+    
+    let mask = CALayer()
+    let fadeInLayer = CALayer()
+    fadeInLayer.contents = fadeIn.cgImage
+    fadeInLayer.frame = CGRect(x: 0, y: 0, width: fadeIn.size.width, height: fadeIn.size.height)
+    mask.addSublayer(fadeInLayer)
+    
+    let fadeOutLayer = CALayer()
+    fadeOutLayer.contents = fadeOut.cgImage
+    fadeOutLayer.frame = CGRect(x: self.peopleInLabView.frame.width - fadeOut.size.width, y: 0, width: fadeOut.size.width, height: self.peopleInLabView.frame.height)
+    mask.addSublayer(fadeOutLayer)
+    
+    let nonFadedLayer = CALayer()
+    nonFadedLayer.contents = nonFaded.cgImage
+    nonFadedLayer.frame = CGRect(x: fadeIn.size.width, y: 0, width: self.peopleInLabView.frame.width - (fadeOut.size.width + fadeIn.size.width), height: self.peopleInLabView.frame.height)
+    mask.addSublayer(nonFadedLayer)
+    
+    mask.frame = self.peopleInLabView.bounds
+    self.peopleInLabView.layer.mask = mask
     
     self.peopleInLabLabel.sizeToFit()
-    self.animatePeopleInLab()
+    if self.peopleInLabLabel.frame.width >= self.peopleInLabView.frame.width {
+      self.peopleInLabLabel.text = text + "  -   "
+      wrapLabel.text = text
+      wrapLabel.textColor = self.peopleInLabLabel.textColor
+      wrapLabel.font = self.peopleInLabLabel.font
+      wrapLabel.sizeToFit()
+      peopleInLabLabel.sizeToFit()
+      wrapLabel.frame = CGRect(x: self.peopleInLabLabel.frame.width, y: self.peopleInLabLabel.frame.origin.y, width: wrapLabel.frame.width, height: wrapLabel.frame.height)
+      peopleInLabLabel.textAlignment = .left
+      wrapLabel.textAlignment = .left
+      
+      self.peopleInLabView.addSubview(wrapLabel)
+      self.animatePeopleInLab()
+    }else{
+      self.peopleInLabLabel.frame = CGRect(x: 0, y: 0, width: self.peopleInLabView.frame.width, height: self.peopleInLabLabel.frame.height)
+    }
   }
   
   func animatePeopleInLab() {
-    let peopleInLabStartFrame = self.peopleInLabLabel.frame
-    let wrapLabelStartFrame = self.wrapLabel.frame
-//    self.peopleInLabLabel.backgroundColor = UIColor.blue
-//    self.wrapLabel.backgroundColor = UIColor.red
     UIView.animate(withDuration: 12.0, delay: 1, options: ([.curveLinear]), animations: {() -> Void in
-      self.peopleInLabLabel.center = CGPoint(x: 0 - self.peopleInLabLabel.bounds.size.width / 2, y: self.peopleInLabLabel.center.y)
-      self.wrapLabel.center = CGPoint(x: 0 + self.wrapLabel.bounds.size.width / 2, y: self.wrapLabel.center.y)
+      let translation = CGAffineTransform(translationX: -self.peopleInLabLabel.bounds.size.width, y: 0)
+      self.wrapLabel.transform = translation
+      self.peopleInLabLabel.transform = translation
     }, completion:  { _ in
-//      self.animatePeopleInLab()
-      self.wrapLabel.frame = wrapLabelStartFrame
-      self.peopleInLabLabel.frame = peopleInLabStartFrame
+      self.wrapLabel.transform = CGAffineTransform(translationX: 0, y: 0)
+      self.peopleInLabLabel.transform = CGAffineTransform(translationX: 0, y: 0)
       self.animatePeopleInLab()
     })
   }
