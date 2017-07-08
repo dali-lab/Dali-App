9ee42b855ddaed28cbbb098f82a91e689b020966
John Kotz <john.lyme@mac.com>
2017-07-08 11:29:58 -0400

    Adding settings saving

--- ios/DALISwift/SettingsViewController.swift
+++ ios/DALISwift/SettingsViewController.swift
@@ -11,16 +11,31 @@ import UIKit
 
 class SettingsViewController: UITableViewController {
 	@IBOutlet weak var signOutCell: UITableViewCell!
+	@IBOutlet weak var enterSwitch: UISwitch!
+	@IBOutlet weak var checkInSwitch: UISwitch!
+	@IBOutlet weak var votingSwitch: UISwitch!
+	@IBOutlet weak var shareSwitch: UISwitch!
+	
+	let defaults: UserDefaults = UserDefaults(suiteName: "Settings")!
 	
 	override func viewDidLoad() {
 		let user = GIDSignIn.sharedInstance().currentUser
 		signOutCell.textLabel?.text = user == nil ? "Sign In" : "Sign out"
+		
+		enterSwitch.isOn = defaults.value(forKey: "enterExitNotification") != nil ? defaults.bool(forKey: "enterExitNotification") : false
+		checkInSwitch.isOn = defaults.value(forKey: "checkInNotification") != nil ? defaults.bool(forKey: "checkInNotification") : true
+		votingSwitch.isOn = defaults.value(forKey: "votingNotification") != nil ? defaults.bool(forKey: "votingNotification") : true
+		shareSwitch.isOn = defaults.value(forKey: "sharePreference") != nil ? defaults.bool(forKey: "sharePreference") : true
+	}
+	
+	@IBAction func switchChanged(_ sender: UISwitch) {
+		defaults.set(sender.isOn, forKey: sender.accessibilityLabel!)
 	}
 	
 	override func numberOfSections(in tableView: UITableView) -> Int {
 		let user = GIDSignIn.sharedInstance().currentUser
 		if let user = user {
-			return userIsAdmin(user: user) ? 3 : 2
+			return userIsAdmin(user: user) ? 4 : 3
 		}else{
 			return 1
 		}
